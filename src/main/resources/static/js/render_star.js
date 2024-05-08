e4146a9bd96f78b2022eb15c79508fa3bf2a3fb1
ThanadolU <dolthanadolu@gmail.com>
2024-05-08 10:02:04 +0700

    remove console.log() in render_star.js

--- src/main/resources/static/js/render_star.js
+++ src/main/resources/static/js/render_star.js
@@ -1,22 +1,22 @@
 function createStarRating(rating) {
-    let stars = '';
+    let stars = [];
     const fullStars = Math.floor(rating); // Full stars
     const halfStar = rating % 1 !== 0; // Check if there's a half star
     const grayStars = 5 - Math.ceil(rating); // Remaining gray stars
 
     // Add full stars
     for (let i = 0; i < fullStars; i++) {
-        stars += '<i class="fa fa-star fa-2x" style="color:orange;"></i>';
+        stars.push('<i class="fa fa-star fa-2x" style="color:orange;"></i>');
     }
 
     // Add half star if needed
     if (halfStar) {
-        stars += '<i class="fa fa-star-half-full fa-2x" style="color:orange;"></i>';
+        stars.push('<i class="fa fa-star-half-full fa-2x" style="color:orange;"></i>');
     }
 
     // Add gray stars
     for (let i = 0; i < grayStars; i++) {
-        stars += '<i class="fa fa-star-o fa-2x" style="color:orange;"></i>';
+        stars.push('<i class="fa fa-star-o fa-2x" style="color:orange;"></i>');
     }
 
     return stars;
@@ -26,27 +26,21 @@ document.addEventListener("DOMContentLoaded", function() {
     const bookElements = document.querySelectorAll('.star-rating');
     bookElements.forEach(bookElement => {
         const averageRating = parseFloat(bookElement.textContent);
-        console.log(averageRating);
-        const starRating = createStarRating(averageRating);
-        console.log(starRating);
-        bookElement.innerHTML = DOMPurify.sanitize(starRating);;
+        const starRating = createStarRating(averageRating).join('');
+        bookElement.innerHTML = starRating;
     });
 });
 
 document.addEventListener("DOMContentLoaded", function() {
     const bookElement = document.getElementById('adminRating');
     const averageRating = parseFloat(bookElement.textContent);
-    console.log(averageRating);
-    const starRating = createStarRating(averageRating);
-    console.log(starRating);
+    const starRating = createStarRating(averageRating).join('');
     bookElement.innerHTML = starRating;
 });
 
 document.addEventListener("DOMContentLoaded", function() {
     const bookElement = document.getElementById('avgUsrRating');
     const averageRating = parseFloat(bookElement.textContent);
-    console.log(averageRating);
-    const starRating = createStarRating(averageRating);
-    console.log(starRating);
+    const starRating = createStarRating(averageRating).join('');
     bookElement.innerHTML = starRating;
 });
